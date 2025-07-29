c4ecc686dda6ce7a081b3beefea3175f101717e6
Johnson Abraham <johnson.abraham@anthology.com>
2025-07-29 22:54:43 +0530

    firebase updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
 import { App } from './App.tsx';
-import { FirebaseAppConfig } from './firebase.ts';
-
-new FirebaseAppConfig();
+import './firebase.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
