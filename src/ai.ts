e7bc99310e77b4f546a34b0f036c9495da7a292d
Johnson Abraham <johnson.abraham@anthology.com>
2025-07-29 20:04:30 +0530

    settings page

--- src/ai.ts
+++ src/ai.ts
@@ -2,15 +2,22 @@ import type { AiResponse } from './types/ai-response.ts';
 import { getFunctions, httpsCallable } from 'firebase/functions';
 import type { AiRequest } from './types/ai-request.ts';
 import { FirebaseError } from 'firebase/app';
+import type { ChatSettings } from './types/chat-settings.ts';
 
-export const getAiResponse = async (
-  prompt: string,
-  systemInstruction: string,
-): Promise<AiResponse | undefined> => {
+export const getAiResponse = async ({
+  prompt,
+  chatSettings,
+}: {
+  prompt: string;
+  chatSettings?: ChatSettings;
+}): Promise<AiResponse | undefined> => {
   const callAi = httpsCallable<AiRequest, AiResponse>(getFunctions(), 'callAi');
 
   try {
-    const response = await callAi({ prompt, systemInstruction });
+    const response = await callAi({
+      prompt,
+      systemInstruction: chatSettings?.systemInstruction,
+    });
     return response.data;
   } catch (err: unknown) {
     if (err instanceof FirebaseError) {
